7e061b8afb32146e726ea182d83373fdde1c2333
Blen Wondimagegn <mekuriablen@gmail.com>
2020-08-20 06:27:15 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,19 @@
+def reverse_each_word_each(string)
+  array = string.split(" ")
+  r_array= []
+  array.each do |string|
+    r_array << string.reverse 
+   end
+    return r_array.join (" ")
+end
+
+def reverse_each_word(string)
+  arr = string.split(" ") 
+  t_array = []
+  arr.collect do|string| 
+    t_array << string.reverse 
+  end
+    t_array.join(" ")
+   
+end
+
