6fe2fcc2999a96619f1b961dab538eafab35db19
Blen Wondimagegn <mekuriablen@gmail.com>
2020-08-23 23:02:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -19,14 +19,4 @@ def reverse_each_word(string)
 end
 
 =======
-def reverse_each_word(string)
-  array = string.split(" ") #turn string into an array
-  test_array = []
-  array.each do|string| #iterate over the array
-    test_array << string.reverse #reverse each word in the array
-  end
-  puts test_array
-   
-  
-end
->>>>>>> 38b8150515368f3a261a29cfe88f8a97f6630faa
+88f8a97f6630faa
