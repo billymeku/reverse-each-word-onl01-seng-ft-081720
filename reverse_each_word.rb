32c6d8684daabdc565c2e16e7082ca7b698115bb
Blen Wondimagegn <mekuriablen@gmail.com>
2020-08-23 22:53:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def reverse_each_word_each(string)
   array = string.split(" ")
   r_array= []
@@ -17,3 +18,15 @@ def reverse_each_word(string)
    
 end
 
+=======
+def reverse_each_word(string)
+  array = string.split(" ") #turn string into an array
+  test_array = []
+  array.each do|string| #iterate over the array
+    test_array << string.reverse #reverse each word in the array
+  end
+  puts test_array
+   
+  
+end
+>>>>>>> 38b8150515368f3a261a29cfe88f8a97f6630faa
